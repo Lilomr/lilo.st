2ad0bbdb6b7769a7325d5415273465be83ac4b61
Murilo <murilomartins847@gmail.com>
2024-09-14 00:11:32 -0300

    Ajuste

--- app.py
+++ app.py
@@ -86,6 +86,14 @@ def index():
     return redirect('/login')
 
 
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected')
 
 
 @socketio.on('message')
